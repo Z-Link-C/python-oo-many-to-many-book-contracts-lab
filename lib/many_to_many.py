32cfabfccf988f146d45ed459505872287896455
Z-Link-C <zachery.croft@gmail.com>
2026-05-09 19:47:10 -0400

    ready to grade

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,59 @@
 class Author:
-    pass
-
+    all=[]
+    def __init__(self,name):
+        self.name=name
+        Author.all.append(self)
+    def contracts(self):
+        return [cont for cont in Contract.all if cont.author==self]
+    def books(self):
+        return[cont.book for cont in Contract.all if cont.author==self]
+    def sign_contract(self,book,date,royalties):
+        return Contract(self,book,date,royalties)
+    def total_royalties(self):
+        return sum(cont.royalties for cont in Contract.all if cont.author==self)   
 
 class Book:
-    pass
-
+    all=[]
+    def __init__(self,title):
+        self.title=title
+        Book.all.append(self)
+    def authors(self):
+        return[cont.author for cont in Contract.all if cont.book==self]
+    def contracts(self):
+        return[contract for contract in Contract.all if contract.book==self]
 
 class Contract:
-    pass
+    all=[]
+    def __init__(self,author,book,date,royalties):
+        self.author=author
+        self.book=book
+        self.date=date
+        self.royalties=int(royalties)
+        Contract.all.append(self)
+    @property
+    def author(self):
+        return self._author
+    @author.setter
+    def author(self,data):
+        if not isinstance(data,Author):
+            raise Exception 
+        self._author=data
+    @property
+    def book(self):
+        return self._book
+    @book.setter
+    def book(self,data):
+        if not isinstance(data,Book):
+            raise Exception 
+        self._book=data
+    @property
+    def date(self):
+        return self._date
+    @date.setter
+    def date(self,data):
+        if not isinstance(data,str):
+            raise Exception 
+        self._date=data
+    @classmethod
+    def contracts_by_date(cls,target):
+        return [c for c in cls.all if c.date==target ]
